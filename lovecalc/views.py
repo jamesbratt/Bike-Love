0761e632ab9237fcc754a9ca8b08ba24b4a2216a
James Bratt <hello@jamesbratt.co.uk>
2017-12-22 01:02:09 +0000

    Raising and handling exceptions

--- lovecalc/views.py
+++ lovecalc/views.py
@@ -1,11 +1,12 @@
 import json
 
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.http import HttpResponse, JsonResponse
 from django.views.generic import TemplateView
 from django.views.generic import View
 
 from strava.views import StravaApi
+from .models import LoveCalculation
 
 
 class StravaTokenCheckMixin(object):
@@ -18,8 +19,8 @@ class StravaTokenCheckMixin(object):
             if code is not '':
                 try:
                     stravaResponse = StravaApi().Oauth(str(code))
-                except:
-                    return HttpResponseRedirect('/')
+                except ValueError as e:
+                    return HttpResponse(e)
 
                 stravaData = stravaResponse.json()
                 request.session['token'] = stravaData['access_token']
@@ -54,49 +55,96 @@ class AchievementResultsView(TemplateView):
  
     template_name = 'lovecalc/achievement-results.html'
 
-    def get_context_data(self, **kwargs):
-        context = super(AchievementResultsView, self).get_context_data(**kwargs)
-        return context
+    def dispatch(self, request, *args, **kwargs):
+        try:
+            LoveCalculation.objects.get(pk=self.kwargs['calculation_id'])
+        except LoveCalculation.DoesNotExist:
+            raise Http404
+
+        return super(AchievementResultsView, self).dispatch(request, *args, **kwargs)
     
 
 class FetchCalculation(View):
 
     def get(self, request, **kwargs):
         if request.is_ajax():
-            return JsonResponse({})
+            try:
+                results = LoveCalculation.objects.get(pk=self.kwargs['calculation_id'])
+            except LoveCalculation.DoesNotExist:
+                response = JsonResponse(
+                    {
+                        'status':'false',
+                        'message':'There was a problem processing this activity.'
+                    }
+                )
+                response.status_code = 500
+                return response
+
+            return JsonResponse(results.results)
     
     
 class CalculateTheLove(StravaTokenCheckMixin, View):
 
     def get(self, request, **kwargs):
         
-        token = self.request.session.get('token')
-        activity = StravaApi().Activity(str(token), self.kwargs['activity_id'])
-        activityObj = activity.json()
+        calculation_activity_id = None
         
-        related_activities = StravaApi().related_activities(str(token), self.kwargs['activity_id'])
+        calculation = LoveCalculation.objects.filter(activity_id=self.kwargs['activity_id'])
         
-        achievements = []
-        
-        total_segments = len(activityObj['segment_efforts']) * (len(related_activities.json()) + 1)
-        
-        achievements.append(
-            {
-                'athlete': self.request.session.get('athlete')['firstname'],
-                'achievements': activityObj['achievement_count']
+        if calculation.count() > 0:
+            calculation_activity_id = str(calculation[0].id)
+        else:
+            token = self.request.session.get('token')
+
+            try:
+                activity = StravaApi().Activity(str(token), self.kwargs['activity_id'])
+            except ValueError as e:
+                return HttpResponse(e)
+            
+            activityObj = activity.json()
+            
+            if activityObj['athlete']['id'] != self.request.session.get('athlete')['id']:
+                return HttpResponse('Not your activity')
+            
+            try:
+                related_activities = StravaApi().related_activities(str(token), self.kwargs['activity_id'])
+            except ValueError as e:
+                return HttpResponse(e)
+            
+            achievements = []
+            
+            total_segments = len(activityObj['segment_efforts']) * (len(related_activities.json()) + 1)
+            
+            achievements.append(
+                {
+                    'athlete': self.request.session.get('athlete')['firstname'],
+                    'achievements': activityObj['achievement_count'],
+                    'activity_id': activityObj['id']
+                }
+            )
+            
+            for activity in related_activities.json():
+                obj = {
+                    'athlete': activity['athlete']['firstname'],
+                    'achievements': activity['achievement_count'],
+                    'activity_id': activity['id']
+                }
+                achievements.append(obj)
+                
+            achievement_sum = sum(achievement['achievements'] for achievement in achievements)
+            performance = round(achievement_sum/total_segments * 100, 0)
+                
+            results = {
+                'results': achievements,
+                'achievement_potential': total_segments,
+                'performance': int(performance)
             }
-        )
-        
-        for activity in related_activities.json():
-            obj = {
-                'athlete': activity['athlete']['firstname'],
-                'achievements': activity['achievement_count']
-            }
-            achievements.append(obj)
             
-        achievement_sum = sum(achievement['achievements'] for achievement in achievements)
-        performance = round(achievement_sum/total_segments * 100, 0)
+            new_calculation = LoveCalculation.objects.create(
+                activity_id=int(activityObj['id']),
+                results=results
+            )
             
-        results = [{'results': achievements, 'achievement_potential': total_segments, 'performance': int(performance)}]
-            
-        return HttpResponse(results)
+            calculation_activity_id = str(new_calculation.id)
+                
+        return HttpResponseRedirect('/activities/results/' + calculation_activity_id)
--- strava/views.py
+++ strava/views.py
@@ -9,22 +9,30 @@ class StravaApi():
         payload = {'client_id':settings.CLIENT_ID, 'client_secret':settings.CLIENT_SECRET, 'code':code}
         getStravaUser = requests.post(settings.OAUTH_URL, verify=False, data=payload)
         response = getStravaUser
+        if response.status_code is not 200:
+            raise ValueError(response.json()['message'])
         return response
 
     def Activities(self, token):
         headers = {'Authorization':'Bearer ' + token}
         getActivities = requests.get(settings.API_URL + 'athlete/activities', verify=False, headers=headers)
         response = getActivities
+        if response.status_code is not 200:
+            raise ValueError(response.json()['message'])
         return response
     
     def Activity(self, token, activity_id):
         headers = {'Authorization':'Bearer ' + token}
         getActivity = requests.get(settings.API_URL + 'activities/' + str(activity_id) + '/?include_all_efforts=true', verify=False, headers=headers)
         response = getActivity
+        if response.status_code is not 200:
+            raise ValueError(response.json()['message'])
         return response
     
     def related_activities(self, token, activity_id):
         headers = {'Authorization':'Bearer ' + token}
         get_related = requests.get(settings.API_URL + 'activities/' + str(activity_id) + '/related', verify=False, headers=headers)
         response = get_related
+        if response.status_code is not 200:
+            raise ValueError(response.json()['message'])
         return response
